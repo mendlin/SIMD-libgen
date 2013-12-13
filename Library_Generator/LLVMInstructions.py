ba1fa9d07ec9ba786883bce1dd7aa73a53069877
logicalmars <bluebird498@gmail.com>
2013-12-12 23:10:25 -0800

    more llvm intrinsics

--- Library_Generator/LLVMInstructions.py
+++ Library_Generator/LLVMInstructions.py
@@ -113,45 +113,6 @@ LLVMBuiltIns = \
 		"return_type":"SIMD_type",
 		"fws":[[2, 4, 8, 16, 32, 64, 128]],
 	},	
-	"simd_constant":\
-	{
-		"signature":["SIMD_type _mm256_set1_epi$fw$(int val)"],
-		"args_type":{"val":"signed_int(32)"},
-		"return_type":"SIMD_type",
-		"fws":[[8, 16, 32]],
-	},
-	"mvmd_fill":\
-	{
-		"signature":["SIMD_type (SIMD_type)_mm256_set1_epi$fw$(int val1)"],
-		"args_type":{"val1":"signed_int(32)"},
-		"return_type":"SIMD_type",
-		"fws":[[8, 16, 32]],
-	},
-	"mvmd_fill4":\
-	{
-		"signature":["SIMD_type (SIMD_type)_mm256_set_epi$fw$(int val1, int val2, int val3, int val4, int val1, int val2, int val3, int val4)"],
-		"args_type":{"val1":"signed_int(32)", "val2":"signed_int(32)", "val3":"signed_int(32)", "val4":"signed_int(32)"},
-		"return_type":"SIMD_type",
-		"fws":[[32]],
-	},
-	"mvmd_fill8":\
-	{
-		"signature":["SIMD_type (SIMD_type)_mm256_set_epi$fw$(int val1, int val2, int val3, int val4, int val5, int val6, int val7, int val8, int val1, int val2, int val3, int val4, int val5, int val6, int val7, int val8)"],
-		"args_type":{"val1":"signed_int(32)", "val2":"signed_int(32)", "val3":"signed_int(32)", "val4":"signed_int(32)", \
-					"val5":"signed_int(32)", "val6":"signed_int(32)", "val7":"signed_int(32)", "val8":"signed_int(32)"},
-		"return_type":"SIMD_type",
-		"fws":[[16]],
-	},
-	"mvmd_fill16":\
-	{
-		"signature":["SIMD_type (SIMD_type)_mm256_set_epi$fw$(int val1, int val2, int val3, int val4, int val5, int val6, int val7, int val8, int val9, int val10, int val11, int val12, int val13, int val14, int val15, int val16, int val1, int val2, int val3, int val4, int val5, int val6, int val7, int val8, int val9, int val10, int val11, int val12, int val13, int val14, int val15, int val16)"],
-		"args_type":{"val1":"signed_int(32)", "val2":"signed_int(32)", "val3":"signed_int(32)", "val4":"signed_int(32)", \
-					"val5":"signed_int(32)", "val6":"signed_int(32)", "val7":"signed_int(32)", "val8":"signed_int(32)", \
-					"val9":"signed_int(32)", "val10":"signed_int(32)", "val11":"signed_int(32)", "val12":"signed_int(32)", \
-					"val13":"signed_int(32)", "val14":"signed_int(32)", "val15":"signed_int(32)", "val16":"signed_int(32)"},
-		"return_type":"SIMD_type",
-		"fws":[[8]],
-	},
 	"bitblock_load_aligned":\
 	{
 		"signature":["SIMD_type _mm256_load_si256(SIMD_type* arg1)"],
